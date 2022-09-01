80154e08189c9223da45b59647f951570cbe14a1
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-08-31 17:47:01 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,31 +17,36 @@ Clothes.create([
     image: "/highwaistjeans.jpg" ,
     price: 89.97,
     name: "High Waist Jeans" ,
-    store_id: 1
+    store_id: 1,
+    category: 'Bottoms'
 },
 {
     image: "/satinmidiskirt.jpg",
     price: 56.80,
     name: "Satin Midi Skirt",
-    store_id: 3
+    store_id: 3,
+    category: 'Bottoms'
 },
 {
     image: "/blackblazer.jpg",
     price: 129.00,
     name: "Black Collared Blazer",
-    store_id: 2
+    store_id: 2,
+    category: 'Jackets'
 },
 {
     image: "/blacksheathdress.jpg",
     price: 62.00,
     name: "Sheath Dress",
-    store_id: 1
+    store_id: 1,
+    category: 'Dresses'
 },
 {
     image: "/stripedbuttonupshirt.jpg",
     price: 195.00,
     name: "Striped Button Up Shirt",
-    store_id: 1
+    store_id: 1,
+    category: 'Tops'
 }
 ])
 

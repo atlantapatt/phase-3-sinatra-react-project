141c33c84924d53824c808e1e3b4dac1e843c6f2
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-09-02 14:11:00 -0700

    update seeds to have reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ Store.create([
 ])
 Clothes.create([
    {
+    id: 1,
     image: "/highwaistjeans.jpg" ,
     price: 89.97,
     name: "High Waist Jeans" ,
@@ -21,6 +22,7 @@ Clothes.create([
     category: 'Bottoms'
 },
 {
+    id: 2,
     image: "/satinmidiskirt.jpg",
     price: 56.80,
     name: "Satin Midi Skirt",
@@ -28,6 +30,7 @@ Clothes.create([
     category: 'Bottoms'
 },
 {
+    id: 3,
     image: "/blackblazer.jpg",
     price: 129.00,
     name: "Black Collared Blazer",
@@ -35,6 +38,7 @@ Clothes.create([
     category: 'Jackets'
 },
 {
+    id: 4,
     image: "/blacksheathdress.jpg",
     price: 62.00,
     name: "Sheath Dress",
@@ -42,11 +46,40 @@ Clothes.create([
     category: 'Dresses'
 },
 {
+    id: 5,
     image: "/stripedbuttonupshirt.jpg",
     price: 195.00,
     name: "Striped Button Up Shirt",
     store_id: 1,
     category: 'Tops'
+}
+])
+
+Reviews.create([
+{
+    rating: 5,
+    comment: "Love the color!",
+    clothes_id: 2
+},
+{
+    rating: 4,
+    comment: "It was a little big.",
+    clothes_id: 1
+},
+{
+    rating: 5,
+    comment: "I love how trendy it is!!",
+    clothes_id: 3
+},
+{
+    rating: 3,
+    comment: "Makes me look boxy",
+    clothes_id: 4
+},
+{
+    rating: 5,
+    comment: "I love this! Perfect for work!",
+    clothes_id: 5
 }
 ])
 

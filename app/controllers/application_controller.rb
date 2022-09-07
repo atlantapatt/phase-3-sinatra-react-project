48c33a000d94c83aa6bd2b7b87556447b4ad201d
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-09-06 19:33:02 -0700

    updated routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/clothes' do 
+    clothes = Clothes.all
+    clothes.to_json
   end
 
+  get '/stores' do 
+    stores = Store.all
+    stores.to_json
+  end
+
+  get '/reviews' do
+    reviews = Reviews.all
+    reviews.to_json
+  end
+
+  get '/users' do
+    users = Users.all
+    users.to_json
+  end
+  
 end

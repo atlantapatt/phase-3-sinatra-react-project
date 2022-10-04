a9c769153f21c916fe40d2d22b52af8fd35608d8
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-10-04 16:18:21 -0700

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+ 
   get '/clothes' do 
-    clothes = Clothes.all
+    clothes = Clothe.all
     clothes.to_json
   end
 
@@ -12,42 +14,52 @@ class ApplicationController < Sinatra::Base
     stores.to_json
   end
 
-  get '/reviews' do
-    reviews = Reviews.all
-    reviews.to_json
+  get '/stores/:id' do 
+    stores = Store.find(params[:id])
+    stores.as_json
   end
 
   get '/users' do
-    users = Users.all
+    users = User.all
     users.to_json
   end
 
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get '/clothes/:id' do 
+    clothes= Clothe.find(params[:id])
+    clothes.to_json(include: :reviews)
+  end
+
   post '/reviews' do
-    review = Reviews.create(
+    review = Review.create(
       rating: params[:rating],
       comment: params[:comment],
       clothes_id: params[:clothes_id],
       user_id: params[:user_id],
-      id: params[:id]
+      # id: params[:id]
     )
     review.to_json
   end
 
   post '/users' do
-   user = Users.create(
+   user = User.create(
     username: params[:username],
-    id: params[:id]
+    # id: params[:id]
    )
    user.to_json
   end
 
   get '/reviews/:id' do 
-    review = Reviews.find(params[:id])
+    review = Review.find(params[:id])
     review.to_json
   end
 
   patch '/reviews/:id' do
-    review = Reviews.find(params[:id])
+    review = Review.find(params[:id])
     review.update(
       rating: params[:rating],
       comment: params[:comment]
@@ -56,7 +68,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/reviews/:id' do 
-    review = Reviews.find(params[:id])
+    review = Review.find(params[:id])
     review.destroy
     review.to_json
   end
